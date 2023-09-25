de1e18825fa530c2e47a70859b5fad1c3b488413
farrukhjon <nazfar1988@gmail.com>
2023-09-25 08:36:09 +0500

    hashtsblr

--- my_observatory/hashtable.c
+++ my_observatory/hashtable.c
@@ -1,81 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-long hash(char *str)
-{
-    int str_length = strlen(str);
+#define TABLE_SIZE 10
 
-    long h = str[0];
-
-    for (int i = 1; i < str_length; i++)
-    {
-        // printf(" %d ", str[i]);
-        int tmp = str[i];
-        if (tmp / 10 < 1)
-        {
-            h = h * 10;
-            h = ((h + tmp) / 2);
-        }
-        else if (tmp / 100 < 1)
-        {
-            h = h * 100;
-            h = ((h + tmp) / 20);
-        }
-        else if (tmp / 1000 < 1)
-        {
-            h = h * 1000;
-            h = ((h + tmp) / 200);
-        }
-    }
-
-    return h/1000000000;//??????
-}
-
-struct node
-{
-    char *name;
+// Define a structure for key-value pairs
+struct KeyValue {
+    char *key;
+    int value;
+    struct KeyValue *next;
 };
 
-struct node has_t[600];
+// Define a hash table as an array of pointers to KeyValue structures
+struct KeyValue *hash_table[TABLE_SIZE];
 
-int main()
-{
-    char *s1 = "arruqjonnazriev";
-    char *s2 = "frruqjonnazriev";
-    char *s3 = "faruqjonnazriev";
-    char *s4 = "farrqjonnazriev";
-    char *s5 = "farrujonnazriev";
-    char *s6 = "farruqonnazriev";
+// Hash function to convert a key into an index
+unsigned int hash(const char *key) {
+    unsigned int hash = 0;
+    while (*key) {
+        hash = (hash * 31) + *key;
+        key++;
+    }
+    return hash % TABLE_SIZE;
+}
 
-    struct node n1;
-    n1.name = s1;
-    int key = hash(s1);
-    has_t[key] = n1;
+// Function to insert a key-value pair into the hash table
+void insert(const char *key, int value) {
+    unsigned int index = hash(key);
+    
+    // Create a new KeyValue node
+    struct KeyValue *new_pair = malloc(sizeof(struct KeyValue));
+    new_pair->key = strdup(key);
+    new_pair->value = value;
+    new_pair->next = NULL;
 
-     struct node n2;
-     n2.name = s2;
-     has_t[hash(s2)] = n2;
+    // Check for collisions and handle them using chaining
+    if (hash_table[index] == NULL) {
+        hash_table[index] = new_pair;
+    } else {
+        struct KeyValue *current = hash_table[index];
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new_pair;
+    }
+}
 
-     struct node n3;
-     n3.name = s3;
-     has_t[hash(s3)] = n3;
+// Function to retrieve the value associated with a key
+int get(const char *key) {
+    unsigned int index = hash(key);
 
-     struct node n4;
-     n4.name = s4;
-     has_t[hash(s4)] = n4;
+    struct KeyValue *current = hash_table[index];
+    while (current != NULL) {
+        if (strcmp(current->key, key) == 0) {
+            return current->value;
+        }
+        current = current->next;
+    }
 
-     struct node n5;
-     n5.name = s5;
-     has_t[hash(s5)] = n5;
+    // Key not found
+    return -1;
+}
 
-     struct node n6;
-     n6.name = s6;
-     has_t[hash(s6)] = n6;
-
-
-    int myhash = hash("frruqjonnazriev");
-
-    printf(" %d \n", myhash);
-
-    printf(" %s \n", has_t[myhash].name);
+int main() {
+    insert("apple", 5);
+    insert("banana", 10);
+    
+    printf("Value for 'apple': %d\n", get("apple"));
+    printf("Value for 'banana': %d\n", get("banana"));
+    printf("Value for 'cherry': %d\n", get("cherry"));
+    
+    return 0;
 }

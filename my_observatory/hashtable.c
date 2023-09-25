be34a80d6417aba8cfc191e27442db5c04b81b78
farrukhjon <nazfar1988@gmail.com>
2023-09-25 16:42:14 +0500

    search 100000000

--- my_observatory/hashtable.c
+++ my_observatory/hashtable.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 // Define the size of the hash table
-#define TABLE_SIZE 1000000
+#define TABLE_SIZE 100000000
 
 // Structure for a key-value pair
 struct KeyValuePair
@@ -12,10 +12,10 @@ struct KeyValuePair
     int value;
 };
 
-struct KeyValuePair *table[TABLE_SIZE];
+char *table[TABLE_SIZE];
 
 // Hash function (this is a simple example; real-world hash functions are more complex)
-unsigned int hash(char *key)
+unsigned int hash(const char *key)
 {
     unsigned int hash = 0;
     while (*key)
@@ -28,24 +28,49 @@ unsigned int hash(char *key)
 // Initialize a hash table
 void createHashTable()
 {
-    for (int i = 0; i < TABLE_SIZE; i++)
-    {
-        table[i] = NULL;
-    }
+    // Use calloc to allocate and initialize memory to zero
+    memset(table, 0, sizeof(char *) * TABLE_SIZE);
 }
 
 // Insert a key-value pair into the hash table
-void insert(char *key, int value)
+void insert(char *key)
 {
-    struct KeyValuePair *kvp = (struct KeyValuePair *)malloc(sizeof(struct KeyValuePair));
-
     unsigned int index = hash(key);
-    kvp->key = strdup(key);
-    kvp->value = value;
 
     // Insert it into the appropriate bucket
-    table[index] = kvp;
+    table[index] = strdup(key);
 }
+
+int isTablePopulated = 0;
+
+void find(char *input)
+{
+    if (isTablePopulated == 0)
+    {
+        printf("%s\n", "entered to populate");
+        createHashTable(); // Initialize the hash table if it's not already
+        for (int i = 0; i < TABLE_SIZE; i++)
+        {
+            char name[10];
+            snprintf(name, sizeof(name), "name%d", i);
+            insert(name);
+        }
+        isTablePopulated = 1;
+    }
+
+    printf("%c", '\n');
+    int hash_index = hash(input);
+
+    if (table[hash_index] == NULL)
+    {
+        printf("%s", "not found\n");
+        exit(1);
+    }
+
+    printf("-> RESULT: %s, hash_index: %d;\n", table[hash_index], hash_index);
+    printf("%c", '\n');
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -57,26 +82,14 @@ int main(int argc, char *argv[])
 
     // get console input arg as search key
     char *input = argv[1];
+    while (strcmp(input, "exit") != 0)
+    {
+        find(input);
 
-    int hash_index = hash("name1");
-    if (table[hash_index] == NULL) {
-        for (int i = 0; i < TABLE_SIZE; i++)
-        {
-            char name[10];
-            snprintf(name, sizeof(name), "name%d", i);
-            insert(name, i);
-        }
+        // Get the next input from the user
+        printf("Enter a search key (or 'exit' to quit): ");
+        scanf("%s", input);
     }
-
-    printf("%c", '\n');
-    hash_index = hash(input);
-    
-    if(table[hash_index] == NULL) {
-        printf("%s", "not found\n");
-        exit(1);
-    }
-    printf("-> RESULT. val: %d, key: %s, hash_index: %d;\n", table[hash_index]->value, table[hash_index]->key, hash_index);
-    printf("%c", '\n');
 
     return 0;
 }
